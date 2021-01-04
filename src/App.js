fbe572a74c77eaacfcf941f6e20cba3de5ce53fb
Riaz Mehmood <engr.riaz2010@gmail.com>
2021-01-04 10:10:25 +0500

    created react app & install router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import {Routes, Route} from 'react-router';
+import {Home} from './components/Home';
+import {About} from './components/About';
+import {Link} from 'react-router-dom';
+import {NotFound} from './components/NotFound';
+import {Products} from './components/Products';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h1>Welcome</h1>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="about">About</Link>
+        <Link to="products">Products</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="about" element={<About/>}/>
+        <Route path="products" element={<Products/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+      
+      </div>
   );
 }
 
